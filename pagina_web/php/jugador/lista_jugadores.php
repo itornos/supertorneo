6c24c067d7fffec0c9fef33f8ea06407cbbce5ed
itornos <100593401+itornos@users.noreply.github.com>
2022-04-17 14:46:33 +0200

    Add files via upload

--- pagina_web/php/jugador/lista_jugadores.php
+++ pagina_web/php/jugador/lista_jugadores.php
@@ -52,7 +52,7 @@ don&amp;apos;t look back., Stop pushing your clients into a corner., $ 29, $ 59,
                     </div>
                     <div class="u-custom-menu u-nav-container">
                         <ul class="u-nav u-unstyled u-nav-1">
-                            <li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="Casa.html" style="padding: 10px 20px;">Casa</a>
+                            <li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="http://torneo.local/casa.html" style="padding: 10px 20px;">Casa</a>
                             </li>
                             <li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="About.html" style="padding: 10px 20px;">About</a>
                             </li>
@@ -65,7 +65,7 @@ don&amp;apos;t look back., Stop pushing your clients into a corner., $ 29, $ 59,
                             <div class="u-inner-container-layout u-sidenav-overflow">
                                 <div class="u-menu-close"></div>
                                 <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2">
-                                    <li class="u-nav-item"><a class="u-button-style u-nav-link" href="Casa.html">Casa</a>
+                                    <li class="u-nav-item"><a class="u-button-style u-nav-link" href="http://torneo.local/casa.html">Casa</a>
                                     </li>
                                     <li class="u-nav-item"><a class="u-button-style u-nav-link" href="About.html">About</a>
                                     </li>
