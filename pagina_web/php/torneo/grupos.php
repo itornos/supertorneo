effc66c1dbd984b71923b2ed1098d4eff9747524
itornos <100593401+itornos@users.noreply.github.com>
2022-05-11 19:27:59 +0200

    Add files via upload

--- pagina_web/php/torneo/grupos.php
+++ pagina_web/php/torneo/grupos.php
@@ -14,13 +14,19 @@ don&amp;apos;t look back., Stop pushing your clients into a corner., $ 29, $ 59,
     <meta name="page_type" content="np-template-header-footer-from-plugin">
     <title>buscador_jugador</title>
     <link rel="stylesheet" href="../../css/nicepage.css" media="screen">
-<link rel="stylesheet" href="../../css/buscador_listaPartido.css" media="screen">
+<link rel="stylesheet" href="../../css/grupos.css" media="screen">
     <script class="u-script" type="text/javascript" src="../../js/jquery.js" defer=""></script>
     <script class="u-script" type="text/javascript" src="../../js/nicepage.js" defer=""></script>
     <meta name="generator" content="Nicepage 4.8.2, nicepage.com">
     <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
     
-    
+    <style>
+      #foto{
+        width: 20px;
+        height: auto;
+        max-height: 30px;
+      }
+    </style>
     
     <script type="application/ld+json">{
 		"@context": "http://schema.org",
@@ -64,35 +70,83 @@ don&amp;apos;t look back., Stop pushing your clients into a corner., $ 29, $ 59,
           </div>
         </nav>
       </div></header>
-
-    <section class="u-align-center u-clearfix u-section-2" id="carousel_d548">
-      <div class="u-clearfix u-sheet u-valign-middle-xs u-sheet-1">
-        <h1 class="u-text u-text-1">LOCAL</h1>
-        <h1 class="u-text u-text-2">VISITANTE</h1>
+    <section style="background-color: white;" class="u-align-center u-clearfix u-palette-5-dark-3 u-section-1" id="carousel_d01c">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h1 style="color: black;" class="u-text u-text-1">FASE DE GRUPOS</h1>
         <?php
 
-        $sql="SELECT * FROM partido";
+        $sql="SELECT DISTINCT grupo FROM equipo order by grupo";
         $result=mysqli_query($conexion,$sql);
 
         while($mostrar=mysqli_fetch_array($result)){
 
         ?>
-        <img src="../../imagenes_equipos_jugadores/<?php echo $mostrar['visitante'] ?>.png" alt="" class="u-image u-image-contain u-image-default u-image-1" data-image-width="354" data-image-height="354">
-        <h1 class="u-text u-text-3"><?php echo strtoupper($mostrar['visitante']) ?></h1>
-        <div class="u-container-style u-group u-shape-rectangle u-group-1">
-          <div class="u-container-layout u-container-layout-1">
-            <img src="../../imagenes_equipos_jugadores/<?php echo $mostrar['local'] ?>.png" alt="" class="u-image u-image-contain u-image-default u-image-2" data-image-width="354" data-image-height="354">
-          </div>
+        <h1 style="color: black;" class="u-align-left u-text u-text-2">GRUPO <?php echo strtoupper($mostrar['grupo']) ?></h1>
+        <div class="u-expanded-width u-table u-table-responsive u-table-1">
+          <table class="u-table-entity">
+            <colgroup>
+              <col width="30%">
+              <col width="8%">
+              <col width="8%">
+              <col width="8%">
+              <col width="8%">
+              <col width="10%">
+              <col width="10%">
+              <col width="10%">
+              <col width="10%">
+            </colgroup>
+            <thead class="u-align-center u-table-header u-white u-table-header-1">
+              <tr style="height: 59px;">
+                <th style="text-align: center;" class="u-align-left u-table-cell u-table-cell-1">EQUIPOS</th>
+                <th class="u-palette-3-base u-table-cell u-table-cell-2">PJ</th>
+                <th class="u-palette-3-base u-table-cell u-table-cell-3"><b>V</b>
+                </th>
+                <th class="u-palette-3-base u-table-cell u-table-cell-4">E</th>
+                <th class="u-palette-3-base u-table-cell u-table-cell-5">D</th>
+                <th class="u-palette-2-base u-table-cell u-table-cell-6">GF</th>
+                <th class="u-palette-2-base u-table-cell u-table-cell-7"><b>GC</b>
+                </th>
+                <th class="u-palette-2-base u-table-cell u-table-cell-8">DG</th>
+                <th class="u-palette-4-base u-table-cell u-table-cell-9">Pts</th>
+              </tr>
+            </thead>
+            <tbody class="u-align-center u-palette-5-dark-3 u-table-body u-table-body-1">
+              <?php
+
+              $sql="SELECT * FROM equipo where grupo = '".$mostrar['grupo']. "'";
+              $result2=mysqli_query($conexion,$sql);
+
+              while($mostrar2=mysqli_fetch_array($result2)){
+
+              ?>
+              <tr style="height: 49px;">
+                <td class="u-align-left u-border-1 u-border-palette-5-dark-2 u-first-column u-table-cell"><img id="foto" src="../../imagenes_equipos_jugadores/<?php echo $mostrar2['nombre'] ?>.png"><?php echo strtoupper($mostrar2['nombre']) ?></td>
+                <td class="u-border-1 u-border-palette-5-dark-2 u-table-cell"><?php echo strtoupper($mostrar2['partidos_jugados']) ?></td>
+                <td class="u-border-1 u-border-palette-5-dark-2 u-table-cell"><?php echo strtoupper($mostrar2['ganado']) ?><br>
+                </td>
+                <td class="u-border-1 u-border-palette-5-dark-2 u-table-cell"><?php echo strtoupper($mostrar2['empate']) ?></td>
+                <td class="u-border-1 u-border-palette-5-dark-2 u-table-cell"><?php echo strtoupper($mostrar2['perdido']) ?></td>
+                <td class="u-border-1 u-border-palette-5-dark-2 u-table-cell"><?php echo strtoupper($mostrar2['gol_favor']) ?></td>
+                <td class="u-border-1 u-border-palette-5-dark-2 u-table-cell"><?php echo strtoupper($mostrar2['gol_contra']) ?></td>
+                <td class="u-border-1 u-border-palette-5-dark-2 u-table-cell"><?php echo strtoupper($mostrar2['dif_goles']) ?></td>
+                <td class="u-border-1 u-border-palette-5-dark-2 u-table-cell"><?php echo strtoupper($mostrar2['puntos']) ?></td>
+              </tr>
+              <?php } ?>
+            </tbody>
+          </table>
         </div>
-        <h1 class="u-text u-text-4"><?php echo strtoupper($mostrar['local']) ?></h1>
-        <h1 class="u-text u-text-5"><?php echo $mostrar['golesLocal'] ?></h1>
-        <h1 class="u-text u-text-6"><?php echo $mostrar['golesVisitante'] ?></h1>
-        <h1 class="u-text u-text-7">-</h1>
+        <br><br><br><br><br>
         <?php } ?>
+        <br><br><br><br>
       </div>
-    </section>    
+    </section>
+    <section class="u-align-center u-clearfix u-section-2" id="carousel_d548">
+      <div class="u-clearfix u-sheet u-valign-middle-xs u-sheet-1"></div>
+    </section>
+    
+    
     <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-e88d"><div class="u-clearfix u-sheet u-sheet-1">
-        <p class="u-small-text u-text u-text-variant u-text-1">Texto de ejemplo xd lol<br>
+        <p class="u-small-text u-text u-text-variant u-text-1">Texto de ejemplo&nbsp; xd lol<br>
         </p>
       </div></footer>
     <section class="u-backlink u-clearfix u-grey-80">
